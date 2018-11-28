8281f4aee148fe64577f1b6009421bbbff14b8fb
Martin Wirth <45354915+LudwigLeiste@users.noreply.github.com>
2018-11-28 11:28:01 +0100

    Add files via upload

--- Hastur's Ritual/Scripts/game01.py
+++ Hastur's Ritual/Scripts/game01.py
@@ -6,11 +6,15 @@ from pytmx import load_pygame
 pygame.init()
 
 
+    ### Zuordnung der Tile-Bezeichnungen ###
+
 grass = 0
 dirt = 1
 stone = 2
 water = 3
 
+
+    ### Die Tile-Map als Liste ###
 
 tilemap = [
         [grass, dirt, water, dirt, stone],
@@ -20,39 +24,59 @@ tilemap = [
         [dirt, dirt, dirt, water, grass]
         ]
 
+    ### Hier eine Liste, für die dodraw-Funktion ###
 
-img = [pygame.image.load("tilepic1.png"),
-       pygame.image.load("tilepic2.png"),
-       pygame.image.load("tilepic3.png"),
-       pygame.image.load("tilepic4.png")]
+img = [pygame.image.load("C:/Users/m.wirth/Desktop/tilepic1.png"),
+       pygame.image.load("C:/Users/m.wirth/Desktop/tilepic2.png"),
+       pygame.image.load("C:/Users/m.wirth/Desktop/tilepic3.png"),
+       pygame.image.load("C:/Users/m.wirth/Desktop/tilepic4.png")]
 
+
+    ### Größe der Tiles und die Größe der Map in Tiles ###
 
 TILESIZE = 50
 MAPWIDTH =  5
 MAPHEIGHT = 5
 
-a = display_width = 250
-b = display_height = 250
+
+    ### Fenster-Größe ###
+
+display_width = TILESIZE * MAPWIDTH
+display_height = TILESIZE * MAPHEIGHT
+
+
+    ### Ein paar Farbbezeichnungen für spätere Verwendung ###
 
 black = (0,0,0)
 white = (255,255,255)
 red = (255,0,0)
 
-player_width = 10
-player_height = 10
 
+    ### Größe des Spielers in Pixeln ###
+
+player_width = 29
+player_height = 35
+
+
+    ### Eigenschaften des PyGame-Fensters ###
 
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption("Hastur's Ritual")
 
 
+    ### Initialisierung der Uhr ###
+
 clock = pygame.time.Clock()
 
 
+    ### Spieler-Grafik laden ###
 
-playerImg = pygame.image.load("Ready Player One test.png")
+playerImg = pygame.image.load("C:/Users/m.wirth/Desktop/Ready Player One test.png")
 
 
+
+    
+    ### Spiel zeichnen, alles ###
 
 def dodraw(playerx, playery):
     gameDisplay.fill(white)
@@ -64,13 +88,15 @@ def dodraw(playerx, playery):
     pygame.display.update()
     clock.tick(60)
     
-    
+
+    ### Text-Ausgabe ###
 
 def text_objects(text, font):
     textSurface = font.render(text, True, black)
     return textSurface, textSurface.get_rect()
 
 
+    ### Erstellt eine Message mit Hilfe von Text-Objects ###
 
 def message_display(text):
     largeText = pygame.font.Font('freesansbold.ttf',25)
@@ -86,6 +112,8 @@ def message_display(text):
 
 
 
+    ### Die Game-Schleife ###
+
 def game_loop():
     x = (display_width * 0.45)
     y = (display_height * 0.8)
@@ -95,14 +123,14 @@ def game_loop():
     gameExit = False
 
     while not gameExit:
-        xold = x
+        xold = x    ### alte Spielerposition wird zwischengespeichert
         yold = y
         
-        for event in pygame.event.get():
+        for event in pygame.event.get():    ### Tastaturabfrage
             if event.type == pygame.QUIT:
                 gameExit = True
             
-            if event.type == pygame.KEYDOWN:
+            if event.type == pygame.KEYDOWN:     ### Bewegungstastendruck
                 if event.key == pygame.K_LEFT:
                     x_change = -2
                 elif event.key == pygame.K_RIGHT:
@@ -112,22 +140,21 @@ def game_loop():
                 elif event.key == pygame.K_DOWN:
                     y_change = 2
         
-            if event.type == pygame.KEYUP:
+            if event.type == pygame.KEYUP:     ### Beim Loslassen der Taste passiert nichts
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     x_change = 0
                 elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                     y_change = 0
-        
-    
-    
-        x += x_change
+          
+        x += x_change     ### Spielerposition verändern
         y += y_change
     
         
-        dodraw(x,y)
+        dodraw(x,y)    ### Hier wird das gesamte Spielfeld neu gezeichnet
     
-        for row in range(MAPWIDTH):
-            print            
+        
+        for row in range(MAPWIDTH):    ### Kollisionsabfrage
+           
             for column in range(MAPHEIGHT):
                 if (x >= (column * TILESIZE) and x <= (column* TILESIZE) + TILESIZE) or ((x+29) >= (column * TILESIZE) and (x+29) <= (column* TILESIZE) + TILESIZE):
                     if (y >= (row * TILESIZE) and y <= (row* TILESIZE) + TILESIZE) or ((y+35) >= (row * TILESIZE) and (y+35) <= (row* TILESIZE) + TILESIZE):
@@ -135,8 +162,7 @@ def game_loop():
                             
                             x = xold
                             y = yold
-
-        
+      
         if (x >= display_width - player_width or x <= 0) or ((x+20) >= display_width - player_width or (x+20) <= 0):
                             x = xold
                             y = yold
@@ -144,9 +170,10 @@ def game_loop():
         if (y >= display_height - player_width or y <= 0) or ((y+26) >= display_height - player_width or (y+26) <= 0):
                             x = xold
                             y = yold
-           
+
+            ### Ende der Kollisionsabfrage ###    
        
         
 game_loop()
-pygame.quit()
+pygame.quit()    ### Pygame ordentlich schließen
 quit()
