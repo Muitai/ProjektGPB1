9ff6870a2ff3c5d6bfb998a9c030445778012b4f
Hearthbaked <nikotin_one@hotmail.de>
2018-11-29 03:38:00 +0100

    Update cwombat_a0.1.py

--- Hastur's Ritual/Scripts/cwombat_a0.1.py
+++ Hastur's Ritual/Scripts/cwombat_a0.1.py
@@ -1,4 +1,4 @@
-## Kampfsystem ##
+#### Kampfsystem                                                                                                       ####
 ## momentaner stand: funktion fragt nach input, goblin ist eingebaut, goblin ist tödbar, sage funktion(nur Temporär)     ##
 ## examine funktion (sagt ob was da ist oder nicht (auch nur temporär), hit funktion (zuschlagen, wird noch modifiziert) ##
 #####importfunktion für Würfelvariabel                                                                                #####
