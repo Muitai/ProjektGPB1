24824afe0c2e03ec3e65f45445c068a16b9a6620
david0343 <45385269+david0343@users.noreply.github.com>
2018-11-29 13:13:17 +0100

    Add files via upload
    Map-design, Spieler Eigenschaften und Fenster eigenschaften/Ausführbarkeit - David

--- Hastur's Ritual/Scripts/World2.py
+++ Hastur's Ritual/Scripts/World2.py
@@ -20,7 +20,7 @@ wall = 2
 water = 3
 
 
-#Die Tile-Map als Liste in bezug auf die Zuordnung der Tiles ###
+#Die Tile-Map als Liste im bezug auf die Zuordnung der Tiles ###
 
 tilemap = ["2222222222222222222222222",
            "2222222220000022222222222",
@@ -46,21 +46,18 @@ tilemap = ["2222222222222222222222222",
 kollisionlist = (wall, water)
 
     ### Größe der Tiles und die Größe der Map in Tiles  in Pixeln ###
-
 TILESIZE = 50
 MAPWIDTH =  len(tilemap[1])
 MAPHEIGHT = len(tilemap)
 
 
     ### Fenster-Größe ###
-
 display_width = TILESIZE * MAPWIDTH
 display_height = TILESIZE * MAPHEIGHT
 
 
 
     ### Größe des Spielers in Pixeln ###
-
 player_width = 29
 player_height = 35
     ### Aktivierte Farben fürs Programieren von Objekten
@@ -69,25 +66,21 @@ white = (255,255,255)
 red = (255,0,0)
 
     ### Eigenschaften des PyGame-Fensters ###
-
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption("Hastur's Ritual")
 
 
     ### Initialisierung der System Uhr ###
-
 clock = pygame.time.Clock()
 
 
     ### Spieler-Grafik laden ###
-
 playerImg = pygame.image.load("Assests/Ready-Player-One-UP1.png")
 
 
 
     
-    ### Spiel zeichnen, aktivierung der Tiles in Reihe und Form ###
-
+### Spiel zeichnen, aktivierung der Tiles in Reihe,Form und Funktion ###
 def dodraw(playerx, playery):
     gameDisplay.fill(white)
     for y in range(MAPHEIGHT):
@@ -95,35 +88,31 @@ def dodraw(playerx, playery):
             gameDisplay.blit(img[int(tilemap[y][x])], (TILESIZE*x,TILESIZE*y))
         
     gameDisplay.blit(playerImg, (playerx,playery))
-    pygame.display.update()
+    pygame.display.update() #Aktualiesiere den Bildschirm
     clock.tick(60) #Maximale FPS begrenzung: 60FPS
     
-
-    ### Text-Ausgabe-Aktivierung ###
-
+### Text-Ausgabe-Aktiviert ###
 def text_objects(text, font):
     textSurface = font.render(text, True, black)
     return textSurface, textSurface.get_rect()
 
 
-    ### Erstellt eine Message mit Hilfe von Text-Objects ###
-
+### Text Form,Größe,Farbe und Position [Text-Objects] ###
 def message_display(text):
     largeText = pygame.font.Font('freesansbold.ttf',25)
     TextSurf, TextRect = text_objects(text, largeText)
     TextRect.center = ((display_width/2),(display_height/2))
     gameDisplay.blit(TextSurf, TextRect)
     
-    pygame.display.update()
+    pygame.display.update() #Aktualiesiere den Bildschirm
     
-    time.sleep(2)
+    time.sleep(2) #Text 2 Sekunden lang anzeigen
     
-    game_loop()
+    game_loop() #Gameschleife starten
 
 
 
-    ### Die Game-Schleife ###
-
+### Die Game-Schleife ###
 def game_loop():
     x = (display_width * 0.45)    ### Startposition des Spielers
     y = (display_height * 0.88)
@@ -151,7 +140,7 @@ def game_loop():
                 elif event.key == pygame.K_DOWN:
                     y_change = 2
         
-            if event.type == pygame.KEYUP:     ### Beim Loslassen der Taste passiert nichts
+            if event.type == pygame.KEYUP:     ### Beim Loslassen der Taste hört die Funktion aus
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     x_change = 0
                 elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
@@ -164,8 +153,8 @@ def game_loop():
         dodraw(x,y)    ### Hier wird das gesamte Spielfeld neu gezeichnet
 
         
-        for row in range(MAPHEIGHT):    ### Kollisionsabfrage
-           
+            ### Kollisionsabfrage ###
+        for row in range(MAPHEIGHT):               
             for column in range(MAPWIDTH):
                 if (x >= (column * TILESIZE) and x <= (column* TILESIZE) + TILESIZE) or ((x+29) >= (column * TILESIZE) and (x+29) <= (column* TILESIZE) + TILESIZE):
                     if (y >= (row * TILESIZE) and y <= (row* TILESIZE) + TILESIZE) or ((y+35) >= (row * TILESIZE) and (y+35) <= (row* TILESIZE) + TILESIZE):
@@ -185,6 +174,6 @@ def game_loop():
             ### Ende der Kollisionsabfrage ###    
        
         
-game_loop()
-pygame.quit()    ### Pygame ordentlich schließen
-quit()
+game_loop()   ###Ende der Game-Schleife###
+pygame.quit() ###Pygame ordentlich schließen###
+quit()        ###Programm beenden###
