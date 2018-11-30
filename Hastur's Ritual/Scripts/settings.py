66443f5133704bd806a2fae2cd65c6ec4eeeee3f
Martin Wirth <45354915+LudwigLeiste@users.noreply.github.com>
2018-11-30 09:37:31 +0100

    Update settings.py

--- Hastur's Ritual/Scripts/settings.py
+++ Hastur's Ritual/Scripts/settings.py
@@ -1,5 +1,16 @@
-# Player settings
+### Player settings
+import pygame
 
+    ### Player speed ###
 player_speed = 300
 player_rot_speed = 250
-playerImg = "Assests/Ready-Player-One-UP1.png"
+
+    ### Size of the player sprite in pixels ###
+player_width = 29
+player_height = 35
+
+    ### Load the images of the player sprite for all four directions in a list ### 
+imgPlayer = [pygame.image.load("Assests/Ready-Player-One-UP1.png"),
+             pygame.image.load("Assests/Ready-Player-One-Down.png"),
+             pygame.image.load("Assests/Ready-Player-One-Left.png"),
+             pygame.image.load("Assests/Ready-Player-One-Right.png")]
